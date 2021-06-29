c1a6ced39aebc897e15f073dc65e3b67006d8487
James Noonan <james@noonanfamily.org>
2021-06-29 12:01:59 +1000

    Parallax

--- lib/story-view.dart
+++ lib/story-view.dart
@@ -11,74 +11,96 @@ class StoryView extends StatefulWidget {
 }
 
 class _StoryViewState extends State<StoryView> {
+  final controller = ScrollController();
+  double parallaxOffset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.removeListener(onScroll);
+  }
+
+  onScroll() {
+    setState(() {
+      parallaxOffset = (-1 / 2) * controller.offset;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     SettingItem setting = SettingData.settingData[widget.story.setting];
 
     return Scaffold(
       backgroundColor: Color.fromRGBO(35, 21, 39, 1),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: Alignment.topCenter,
-            fit: BoxFit.contain,
-            image: AssetImage(setting.getMainImage()),
+      body: Stack(
+        children: [
+          Transform(
+            child: Image.asset(setting.getMainImage()),
+            alignment: Alignment(0, -1),
+            transform: Matrix4.translationValues(0, parallaxOffset, 0),
           ),
-        ),
-        child: ListView(
-          children: [
-            SizedBox.fromSize(
-              size: Size(MediaQuery.of(context).size.width,
-                  MediaQuery.of(context).size.width * (12 / 16)),
-            ),
-            Image.asset(setting.getParallaxImage()),
-            Container(
-              transform: Matrix4.translationValues(0.0, -20.0, 0.0),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(35, 21, 39, 1),
+          ListView(
+            controller: controller,
+            children: [
+              SizedBox.fromSize(
+                size: Size(MediaQuery.of(context).size.width,
+                    MediaQuery.of(context).size.width * (12 / 16)),
               ),
-              child: SafeArea(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 30, right: 30, bottom: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.story.title,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(bottom: 20),
-                        child: Text(
-                          "by " + widget.story.author,
+              Image.asset(setting.getParallaxImage()),
+              Container(
+                transform: Matrix4.translationValues(0.0, -10.0, 0.0),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(35, 21, 39, 1),
+                ),
+                child: SafeArea(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30, right: 30, bottom: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.story.title,
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 18,
+                            fontSize: 30,
                           ),
                         ),
-                      ),
-                      Container(
-                        child: Text(
-                          widget.story.body,
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            color: Colors.white,
-                            height: 1.3,
-                            fontSize: 19,
+                        Container(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Text(
+                            "by " + widget.story.author,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Container(
+                          child: Text(
+                            widget.story.body,
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              color: Colors.white,
+                              height: 1.3,
+                              fontSize: 19,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
